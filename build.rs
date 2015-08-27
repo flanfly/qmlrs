edfc4893b6bb504772e6fbc8baf1a2abbbe9500e
seu <seu@panopticon.re>
2015-08-27 20:46:36 +0200

    build with msvc

--- build.rs
+++ build.rs
@@ -7,22 +7,24 @@ use std::path::PathBuf;
 
 fn main() {
     let wcd = env::current_dir().unwrap();
-    let mut build = PathBuf::from(&wcd.join("ext/libqmlrswrapper/build"));
-
+    let build = PathBuf::from(&wcd.join("ext/libqmlrswrapper/build"));
     let _ = fs::create_dir_all(&build);
 
-    let mut myargs = vec![".."] ;
-    let is_msys = env::var("MSYSTEM").is_ok() ;
     if cfg!(windows) {
-        myargs.push("-GMSYS Makefiles") ;
+        Command::new("cmake")
+            .args(&vec!["-GVisual Studio 12 2013 Win64",".."])
+            .current_dir(&build)
+            .status().and_then(|x| Ok(x.success()) ).unwrap_or_else(|e| {
+                panic!("Failed to run cmake: {}", e);
+            });
+    } else {
+        Command::new("cmake")
+            .args(&vec![".."])
+            .current_dir(&build)
+            .status().and_then(|x| Ok(x.success()) ).unwrap_or_else(|e| {
+                panic!("Failed to run cmake: {}", e);
+            });
     }
-
-    println!("{}",Command::new("cmake")
-        .args(&myargs)
-        .current_dir(&build)
-        .status().and_then(|x| Ok(x.success()) ).unwrap_or_else(|e| {
-            panic!("Failed to run cmake: {}", e);
-        }));
 
     Command::new("cmake")
         .args(&vec!["--build","."])
@@ -31,11 +33,20 @@ fn main() {
             panic!("Failed to run build: {}", e);
         });
 
-    if cfg!(windows) && is_msys {
-        println!("cargo:rustc-link-search=native={}\\system32",env::var("WINDIR").unwrap());
-    }
     println!("cargo:rustc-link-lib=static=qmlrswrapper");
-    println!("cargo:rustc-link-lib=dylib=stdc++");
-    println!("cargo:rustc-link-search=native={}",build.display());
-    pkg_config::find_library("Qt5Core Qt5Gui Qt5Qml Qt5Quick").unwrap();
+
+    if cfg!(windows) {
+        println!("cargo:rustc-link-search=native={}\\system32",env::var("WINDIR").unwrap());
+        println!("cargo:rustc-link-search=native={}\\Debug",build.display());
+        println!("cargo:rustc-link-search=native={}\\lib",env::var("QTDIR").unwrap());
+
+        println!("cargo:rustc-link-lib=dylib=Qt5Core");
+        println!("cargo:rustc-link-lib=dylib=Qt5Gui");
+        println!("cargo:rustc-link-lib=dylib=Qt5Qml");
+        println!("cargo:rustc-link-lib=dylib=Qt5Quick");
+    } else {
+        println!("cargo:rustc-link-search=native={}{}",build.display(),if cfg!(windows) { "\\Debug" } else { "" });
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+        pkg_config::find_library("Qt5Core Qt5Gui Qt5Qml Qt5Quick").unwrap();
+    }
 }
